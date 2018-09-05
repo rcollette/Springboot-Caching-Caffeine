fdbec957d879cf416beb3a78f60257a4aaefeec6
Richard Collette <richard.collette@pb.com>
2018-09-05 00:27:37 -0400

    Remove unused import

--- src/main/java/com/example/demo/CacheConfiguration.java
+++ src/main/java/com/example/demo/CacheConfiguration.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
